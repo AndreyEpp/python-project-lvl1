0fc9a56720f77d3c0c1ab5043d9a4cca8862c3b5
AndreyEpp <epp@mayak.help>
2022-04-29 12:53:26 +0300

    add missing 'to'

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,7 @@
 import prompt
 
 def brain_games():
-    print('Welcome the Brain Games!')
+    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, my friend {name}!')
 
