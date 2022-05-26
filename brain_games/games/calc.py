0ad9449a3a2e343e5c1cab69765f8b6e9e799cb9
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 10:42:38 +0300

    Update calc.py

---
+++ brain_games/games/calc.py
@@ -0,0 +1,45 @@
+import prompt
+import random
+
+
+def brain_calc():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, {}!'.format(name))
+    print('What is the result of the expression?')
+    i = 0
+    flag = True
+    while i < 3:
+        num1 = random.randint(0, 100)
+        num2 = random.randint(0, 100)
+        foo = ['+', '-', '*']
+        oper = random.choice(foo)
+        if oper == '+':
+            check = num1 + num2
+        elif oper == '-':
+            check = num1 - num2
+        else:
+            check = num1 * num2
+        print('Question: {} {} {}'.format(num1, oper, num2))
+        answer = prompt.string('Your answer: ')
+        if int(answer) == check:
+            print('Correct!')
+            flag = True
+            i += 1
+        else:
+            flag = False
+            break
+
+    if flag:
+        print('Congratulations, {}!'.format(name))
+    else:
+        print("'{}' is wrong answer ;(.\n"
+              "Let's try again, {}!".format(answer, name))
+
+
+def main():
+    brain_calc()
+
+
+if __name__ == '__main__':
+    main()
