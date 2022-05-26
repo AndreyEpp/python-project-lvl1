b6d1de465e0edb359f2e38e62be8f3aefd7e24f1
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 10:44:45 +0300

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,44 +1,12 @@
-import prompt
-import random
+"""Brain calc game."""
 
-
-def brain_calc():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
-    print('What is the result of the expression?')
-    i = 0
-    flag = True
-    while i < 3:
-        num1 = random.randint(0, 100)
-        num2 = random.randint(0, 100)
-        foo = ['+', '-', '*']
-        oper = random.choice(foo)
-        if oper == '+':
-            check = num1 + num2
-        elif oper == '-':
-            check = num1 - num2
-        else:
-            check = num1 * num2
-        print('Question: {} {} {}'.format(num1, oper, num2))
-        answer = prompt.string('Your answer: ')
-        if int(answer) == check:
-            print('Correct!')
-            flag = True
-            i += 1
-        else:
-            flag = False
-            break
-
-    if flag:
-        print('Congratulations, {}!'.format(name))
-    else:
-        print("'{}' is wrong answer ;(.\n"
-              "Let's try again, {}!".format(answer, name))
+from brain_games.engine import run
+from brain_games.games import brain_calc
 
 
 def main():
-    brain_calc()
+    """Run calc game."""
+    run(brain_calc)
 
 
 if __name__ == '__main__':
