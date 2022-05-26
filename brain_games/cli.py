b2355b3358e63720453e1424939531fbe08cdcee
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 12:15:05 +0300

    Update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,3 +1,5 @@
+'''User input'''
+
 import prompt
 
 
@@ -6,3 +8,8 @@ def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
+
+
+def get_user_answer():
+    answer = prompt.string('Your answer: ')
+    return answer
