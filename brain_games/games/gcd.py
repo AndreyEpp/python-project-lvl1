d36f8938bf2f3b32edd8c6116913374f26b6640a
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 15:46:25 +0300

    Update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -12,5 +12,5 @@ def get_question_and_answer():
     num2 = random.randint(0, 100)
     check = math.gcd(num1, num2)
     print(f'Question: {num1} {num2}')
-    user_answer = prompt.string('Your answer: ')
+    user_answer = int(prompt.string('Your answer: '))
     return check, user_answer
