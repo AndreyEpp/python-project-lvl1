2f9644c14b9fd10a1d985ce6539300422a18d670
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 15:30:30 +0300

    Update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -8,32 +8,14 @@ from brain_games.engine import run_game
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def get_question_and_answer():
-    yes_no = 'no'
-    while i < 3:
-        num = random.randint(0, 100)
-        print('Question: {}'.format(num))
-        answer = prompt.string('Your answer: ')
-        if num % 2 == 0 and answer == 'yes' or num % 2 != 0 and answer == 'no':
-            print('Correct!')
-            i = i + 1
-            flag = True
-        else:
-            if answer == 'no':
-                yes_no = 'yes'
-            else:
-                yes_no = 'no'
-            flag = False
-            break
-    if flag:
-        print('Congratulations, {}!'.format(name))
+    check = 'no'
+    num = random.randint(0, 100)
+    print('Question: {}'.format(num))
+    answer = prompt.string('Your answer: ')
+    if num % 2 == 0 and user_answer == 'yes' or num % 2 != 0 and user_answer == 'no':
+        check = 'yes'
     else:
-        print("'{}' is wrong answer ;(. Correct answer was '{}'\n"
-              "Let's try again, {}!".format(answer, yes_no, name))
-
-
-def main():
-    brain_even()
-
-
-if __name__ == '__main__':
-    main()
+        check = 'no'
+    return check, user_answer
+    
+    
