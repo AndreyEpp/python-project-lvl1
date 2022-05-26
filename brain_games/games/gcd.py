4cbcfef4833102e6fce63bc930a8d4e9ae53c878
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 15:44:07 +0300

    Update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -10,11 +10,7 @@ DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 def get_question_and_answer():
     num1 = random.randint(0, 100)
     num2 = random.randint(0, 100)
-    nod = math.gcd(num1, num2)
+    check = math.gcd(num1, num2)
     print(f'Question: {num1} {num2}')
     user_answer = prompt.string('Your answer: ')
-    if int(user_answer) == nod:
-        check = 'yes'
-    else:
-        check = 'no'
     return check, user_answer
