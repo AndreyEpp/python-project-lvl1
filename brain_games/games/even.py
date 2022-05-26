3671df611ea5ddf3f042cad2013a92aeb8ca198c
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 16:40:53 +0300

    Update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -10,12 +10,11 @@ DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 def get_question_and_answer():
     check = 'no'
     num = random.randint(0, 100)
-    print('Question: {}'.format(num))
+    print(f'Question: {num}')
     user_answer = prompt.string('Your answer: ')
-    if num % 2 == 0 and user_answer == 'yes' or num % 2 != 0 and user_answer == 'no':
+    if num % 2 == 0:
         check = 'yes'
     else:
         check = 'no'
     return check, user_answer
-    
-    
+       
