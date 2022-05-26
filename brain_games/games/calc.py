33b682bd9413f1840afa396a791dabfa3b842519
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 13:26:45 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,52 +1,66 @@
+'''тут как то создаем пару вопрос и ответ.'''
+
 import prompt
 import random
+from brain_games.engine import run_game 
 
 
 DESCRIPTION = 'What is the result of the expression?'
 
 def get_question_and_answer():
-    тут как то создаем пару вопрос и ответ.
-    return question, answer
-
-
-def brain_calc():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
-    print('What is the result of the expression?')
-    i = 0
-    flag = True
-    while i < 3:
-        num1 = random.randint(0, 100)
-        num2 = random.randint(0, 100)
-        foo = ['+', '-', '*']
-        oper = random.choice(foo)
-        if oper == '+':
-            check = num1 + num2
-        elif oper == '-':
-            check = num1 - num2
-        else:
-            check = num1 * num2
-        print('Question: {} {} {}'.format(num1, oper, num2))
-        answer = prompt.string('Your answer: ')
-        if int(answer) == check:
-            print('Correct!')
-            flag = True
-            i += 1
-        else:
-            flag = False
-            break
-
-    if flag:
-        print('Congratulations, {}!'.format(name))
+    num1 = random.randint(0, 100)
+    num2 = random.randint(0, 100)
+    foo = ['+', '-', '*']
+    oper = random.choice(foo)
+    if oper == '+':
+        check = num1 + num2
+    elif oper == '-':
+        check = num1 - num2
     else:
-        print("'{}' is wrong answer ;(.\n"
-              "Let's try again, {}!".format(answer, name))
+        check = num1 * num2
+    print('Question: {} {} {}'.format(num1, oper, num2))
+    answer = prompt.string('Your answer: ')    
+    return question, user_answer
 
 
-def main():
-    brain_calc()
+# def brain_calc():
+#     print('Welcome to the Brain Games!')
+#     name = prompt.string('May I have your name? ')
+#     print('Hello, {}!'.format(name))
+#     print('What is the result of the expression?')
+#     i = 0
+#     flag = True
+#     while i < 3:
+#         num1 = random.randint(0, 100)
+#         num2 = random.randint(0, 100)
+#         foo = ['+', '-', '*']
+#         oper = random.choice(foo)
+#         if oper == '+':
+#             check = num1 + num2
+#         elif oper == '-':
+#             check = num1 - num2
+#         else:
+#             check = num1 * num2
+#         print('Question: {} {} {}'.format(num1, oper, num2))
+#         answer = prompt.string('Your answer: ')
+#         if int(answer) == check:
+#             print('Correct!')
+#             flag = True
+#             i += 1
+#         else:
+#             flag = False
+#             break
+
+#     if flag:
+#         print('Congratulations, {}!'.format(name))
+#     else:
+#         print("'{}' is wrong answer ;(.\n"
+#               "Let's try again, {}!".format(answer, name))
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     brain_calc()
+
+
+# if __name__ == '__main__':
+#     main()
