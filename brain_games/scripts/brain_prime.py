8e622977c952a81b185565b23ca08c73ff6f3987
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 12:03:08 +0300

    Update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,41 +1,11 @@
-import prompt
-import random
-from brain_games.cli import welcome_user
+"""Brain prime game."""
 
-
-def brain_prime():
-    name = welcome_user()
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    flag = True
-    i = 0
-    while i < 3:
-        num = random.randint(0, 100)
-        k = False
-        print(f'Question: {num}')
-        answer = prompt.string('Your answer: ')
-        max_range = num // 2 + 2
-        for delitel in range(2, max_range):
-            if (num % delitel == 0 and delitel < num):
-                k = False
-                break
-            else:
-                k = True
-        if answer == 'yes' and k or answer == 'no' and not k:
-            print('Correct!')
-            flag = True
-            i += 1
-        else:
-            flag = False
-            break
-
-    if flag:
-        print(f'Congratulations, {name}!')
-    else:
-        print(f"'{answer}' is wrong answer ;(.\nLet's try again, {name}!")
+from brain_games.engine import run_game
+from brain_games.games import prime
 
 
 def main():
-    brain_prime()
+    run_game(prime)
 
 
 if __name__ == '__main__':
