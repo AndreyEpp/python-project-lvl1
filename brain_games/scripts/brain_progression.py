9a2b7013d965fa6143f39b9ddf279303f95764fe
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 12:04:02 +0300

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,45 +1,11 @@
-import prompt
-import random
+"""Brain progression game."""
 
-
-def brain_progression():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {name}!')
-    print('What number is missing in the progression?')
-    i = 0
-    flag = True
-    while i < 3:
-        list_a = []
-        num_start = random.randint(1, 10)
-        len_list = random.randint(5, 10)
-        index_zameny = random.randint(0, len_list)
-        s = ''
-        for num in range(0, len_list + 1):
-            list_a.append(num_start * (num + 1))
-            if index_zameny == num:
-                s = s + '.. '
-            else:
-                s = s + str(num_start * (num + 1)) + ' '
-        value_zameny = list_a[index_zameny]
-        print(f'Question: {s}')
-        answer = prompt.string('Your answer: ')
-        if int(answer) == value_zameny:
-            print('Correct!')
-            flag = True
-            i += 1
-        else:
-            flag = False
-            break
-
-    if flag:
-        print(f'Congratulations, {name}!')
-    else:
-        print(f"'{answer}' is wrong answer ;(.\nLet's try again, {name}!")
+from brain_games.engine import run_game
+from brain_games.games import progression
 
 
 def main():
-    brain_progression()
+    run_game(progression)
 
 
 if __name__ == '__main__':
