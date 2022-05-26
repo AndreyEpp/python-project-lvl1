b9544a87eda3fcb56685da7549dc25c3850276e2
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 16:13:59 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,46 +1,25 @@
+'''Brain progression game functions.'''
+
 import prompt
 import random
+from brain_games.engine import run_game
 
+DESCRIPTION = 'What number is missing in the progression?'
 
-def brain_progression():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {name}!')
-    print('What number is missing in the progression?')
-    i = 0
-    flag = True
-    while i < 3:
-        list_a = []
-        num_start = random.randint(1, 10)
-        len_list = random.randint(5, 10)
-        index_zameny = random.randint(0, len_list)
-        s = ''
-        for num in range(0, len_list + 1):
-            list_a.append(num_start * (num + 1))
-            if index_zameny == num:
-                s = s + '.. '
-            else:
-                s = s + str(num_start * (num + 1)) + ' '
-        value_zameny = list_a[index_zameny]
-        print(f'Question: {s}')
-        answer = prompt.string('Your answer: ')
-        if int(answer) == value_zameny:
-            print('Correct!')
-            flag = True
-            i += 1
+def get_question_and_answer():
+    list_a = []
+    num_start = random.randint(1, 10)
+    len_list = random.randint(5, 10)
+    index_zameny = random.randint(0, len_list)
+    s = ''
+    for num in range(0, len_list + 1):
+        list_a.append(num_start * (num + 1))
+        if index_zameny == num:
+            s = s + '.. '
         else:
-            flag = False
-            break
-
-    if flag:
-        print(f'Congratulations, {name}!')
-    else:
-        print(f"'{answer}' is wrong answer ;(.\nLet's try again, {name}!")
-
-
-def main():
-    brain_progression()
-
-
-if __name__ == '__main__':
-    main()
+            s = s + str(num_start * (num + 1)) + ' '
+    check = int(list_a[index_zameny])
+    print(f'Question: {s}')
+    user_answer = prompt.string('Your answer: ')
+    return check, user_answer
+    
