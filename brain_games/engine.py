bfa1f6a69fc9a97a2aeb20a515584429c11486bd
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-05-26 13:26:23 +0300

    Update engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,18 +1,29 @@
-''' Game engine functions.'''
+''' Game engine functions.
+тут далее уже вся логика, цикл и тут используем
+    логику описанную для каждой игры.'''
 
 from random import randint
-from brain_games.cli import get_user_answer, get_user_name
+from brain_games.cli import welcome_user, user_answer
 
 NUMBER_OF_ROUNDS = 3
 
 
 def run_game(game):
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
-    i = 0
-    flag = True
-    тут далее уже вся логика, цикл и тут используем
-    логику описанную для каждой игры.
+    name = welcome_user()
     print(game.DESCRIPTION)
+    correct_answers = 0
+    flag = True
+    while correct_answers < NUMBER_OF_ROUNDS:
+        if user_answer == check:
+            print('Correct!')
+            flag = True
+            correct_answers += 1
+        else:
+            flag = False
+            break
+
+    if flag:
+        print(f'Congratulations, {name}!')
+    else:
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{check}'\nLet's try again, {name}!")
     question, correct_answer = game.get_question_and_answer()
