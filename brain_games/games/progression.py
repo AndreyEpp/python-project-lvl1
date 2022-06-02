2879f153583745de264a88d76763c97de14d2d01
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-06-02 13:18:37 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,7 +10,7 @@ def get_question_and_answer():
     num_start = random.randint(1, 10)
     len_list = random.randint(5, 10)
     step = random.randint(1, 10)
-    index_zameny = random.randint(0, len_list)
+    index_zameny = random.randint(0, len_list - 1)
     progression = list(range(num_start, (num_start + len_list * step), step))
     answer, progression[index_zameny] = progression[index_zameny], '..'
     question = " ".join(map(str,progression))
