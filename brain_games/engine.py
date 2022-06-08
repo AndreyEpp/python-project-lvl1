f516c9eff8c2f70608df94fd68ca705f720dfe70
AndreyEpp <epp@mayak.help>
2022-06-08 15:47:19 +0300

    fix engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,29 +3,24 @@
 from brain_games.cli import welcome_user
 import prompt
 
-NUMBER_OF_ROUNDS = 3
-
 
 def run_game(game):
     name = welcome_user()
     print(game.DESCRIPTION)
-    correct_answers = 0
-    flag = True
-    while correct_answers < NUMBER_OF_ROUNDS:
+    game_rounds_count = 3
+    for round_number in range(0, game_rounds_count):
         question, answer = game.get_question_and_answer()
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
         if user_answer == answer:
             print('Correct!')
             flag = True
-            correct_answers += 1
         else:
+            print(f"'{user_answer}' is wrong answer ;(."
+                  f"Correct answer was '{answer}'\n"
+                  f"Let's try again, {name}!")
             flag = False
             break
 
     if flag:
         print(f'Congratulations, {name}!')
-    else:
-        print(f"'{user_answer}' is wrong answer ;(."
-              f"Correct answer was '{answer}'\n"
-              f"Let's try again, {name}!")
