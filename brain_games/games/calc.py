dc00173bd3300a678055bcc5b4be6dd0898858ea
AndreyEpp <99253617+AndreyEpp@users.noreply.github.com>
2022-06-08 15:50:39 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,6 +1,7 @@
 '''Brain calc game functions.'''
 
 import random
+import operator
 
 
 DESCRIPTION = 'What is the result of the expression?'
@@ -9,13 +10,12 @@ DESCRIPTION = 'What is the result of the expression?'
 def get_question_and_answer():
     num1 = random.randint(0, 100)
     num2 = random.randint(0, 100)
-    foo = ['+', '-', '*']
-    oper = random.choice(foo)
-    if oper == '+':
-        answer = num1 + num2
-    elif oper == '-':
-        answer = num1 - num2
-    else:
-        answer = num1 * num2
+    operations = {
+   '+': operator.add,
+   '-': operator.sub,
+   '*': operator.mul,
+    }
+    operation = random.choice(list(operations.keys()))
+    answer = str(operations[operation](num1, num2))
     question = f'{num1} {oper} {num2}'
-    return question, str(answer)
+    return question, answer
